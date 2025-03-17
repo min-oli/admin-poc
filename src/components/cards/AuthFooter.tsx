552fd159064f1b8a6fee4612a687ec034de63e1d
min-oli <qwes123@cj.net>
2025-03-17 14:36:10 +0900

    feat: footer를 수정한다.

--- src/components/cards/AuthFooter.tsx
+++ src/components/cards/AuthFooter.tsx
@@ -1,6 +1,5 @@
 // material-ui
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 
@@ -9,37 +8,17 @@ import Stack from '@mui/material/Stack';
 export default function AuthFooter() {
   return (
     <Container maxWidth="xl">
-      <Stack
-        direction={{ xs: 'column', sm: 'row' }}
-        sx={{ gap: 2, justifyContent: { xs: 'center', sm: 'space-between', textAlign: { xs: 'center', sm: 'inherit' } } }}
-      >
-        <Typography variant="subtitle2" color="secondary">
-          © Made with love by Team{' '}
-          <Link href="https://codedthemes.com/" target="_blank" underline="hover">
-            CodedThemes
-          </Link>
-        </Typography>
-
-        <Stack direction={{ xs: 'column', sm: 'row' }} sx={{ gap: { xs: 1, sm: 3 }, textAlign: { xs: 'center', sm: 'inherit' } }}>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://mui.com/store/terms/"
-            target="_blank"
-            underline="hover"
-          >
-            Terms and Conditions
+      <Stack direction="column" justifyContent="center" alignItems="center" textAlign="center">
+        <Stack alignItems="center" textAlign="center">
+          <Typography variant="body2" color="secondary">
+            This system can only be used by Olive Young executives and employees and may be punished by relevant laws and regulations for
+            illegal access and use.
           </Typography>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://mui.com/legal/privacy/"
-            target="_blank"
-            underline="hover"
-          >
-            Privacy Policy
+        </Stack>
+
+        <Stack alignItems="center" textAlign="center">
+          <Typography variant="body2" color="secondary">
+            Copyright ⓒ 2025 CJ OLIVE YOUNG. All rights reserved.
           </Typography>
         </Stack>
       </Stack>
